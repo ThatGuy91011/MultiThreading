77f2ac17b3e959499cab72361c8f0d6d170e84b5
Kirk Walls <kirwalls@uat.edu>
2020-04-05 22:06:20 -0400

    Final Commit

--- MultiThreading/Source.cpp
+++ MultiThreading/Source.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <thread>
+#include <Windows.h>
 using namespace std;
 
 void LaunchPad();
 
+//Integer that holds the total number of rockets launched
 static int totalRocketsLaunched;
 
 static bool abortButton = false;
@@ -15,44 +17,51 @@ int main()
 
 	cout << "*******Moon Base (Threading)********" << endl;
 
-
+	//Tells the user the ID of the main thread
 	cout << "Main Thread ID: " << this_thread::get_id() << endl;
 	cout << "To start launching rockets, ";
 	system("pause");
 
-
+	//Integer that holds the total number of rockets launched from Mission Control
 	int mcLaunchedRockets = 0;
-	mcLaunchedRockets++;
-	totalRocketsLaunched++;
-	cout << "Launching rocket #" << mcLaunchedRockets << " from Mission Control." << endl;
-	cout << "This is rocket #" << totalRocketsLaunched << " overall.\n";
 
+	//Launches the first rocket
+	cout << "Launching rocket #" << ++mcLaunchedRockets << " from Mission Control." << endl;
+	cout << "\n\tThis is rocket #" << ++totalRocketsLaunched << " overall.\n\n";
 	
 	//Start a brand new thread
 	thread thread_LaunchPad(LaunchPad);
 
-
-	//Abort Button
-	cout << "To abort rocket launch, press space bar.\n";
-	system("pause");
-	abortButton = true;
-	cout << "\n\tLaunches Aborted\n";
+	//Join the new thread
 	thread_LaunchPad.join();
 
+	cout << "\n\n\t\tThank you for using Missiles R Us";
 	return 0;
 }
 
 void LaunchPad()
 {
+	//Integer that holds the total number of rockets launched from Launch Pad
 	int lpLaunchedRockets = 0;
 
-	cout << "Launch Pad Thread ID: " << this_thread::get_id() << endl;
+	//Tells the user the ID of the secondary thread
+	cout << "Launch Pad Thread ID: " << this_thread::get_id() << endl << endl;
+
+	//Abort Button Prompt
+	cout << "\t***To abort rocket launch, press space bar***\n\n";
 
 	while (abortButton != true)
 	{
+		//Continuously launches rockets until abort button is pressed
 		cout << "Launching local rocket #" << ++lpLaunchedRockets << " from launch pad.\n";
-		cout << "This is rocket #" << ++totalRocketsLaunched << " overall.\n";
+		cout << "\n\tThis is rocket #" << ++totalRocketsLaunched << " overall.\n\n";
+
 		//Simulate waiting time
-		this_thread::sleep_for(5s);
+		this_thread::sleep_for(3s);
+		if (GetKeyState(VK_SPACE))
+		{
+			abortButton = true;
+			cout << "\n\tLaunches Aborted\n";
+		}
 	}
 }
